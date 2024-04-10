56c9271ac324502e2494aae359fab9731cb06f3a
Bran van der Meer <bran.van.der.meer@protonmail.com>
2024-04-10 23:59:59 +0100

    feat: Correct sign-up full page with number of people

--- src/pages/Signup/full.jsx
+++ src/pages/Signup/full.jsx
@@ -1,17 +1,28 @@
+import { ref } from 'firebase/database'
+import { db } from '../../util/firebase'
+import { useObjectVal } from 'react-firebase-hooks/database'
+
 export default function SignUpFullPage() {
+  const [limit] = useObjectVal(ref(db, 'attendeesLimit'))
+
   return (
     <article>
       <h1>We’re full</h1>
       <p className="introduction">OH NO!</p>
       <p>
-        We’re full for this year! Only 18 people can fit in the chalet, so the
-        registration is now closed.
+        We’re full for this year!{' '}
+        {limit ? (
+          <>
+            Only {limit} people can fit in the chalet, so the registration is
+            now closed.
+          </>
+        ) : (
+          <>Registration is closed.</>
+        )}
       </p>
       <p>
-        Send an email to{' '}
-        <a href="mailto:miraski2025@proton.me">miraski2025@proton.me</a> to
-        claim a spot on the waiting list. We’ll get in touch when a spot opens
-        up.
+        If you think you really deserve a spot, or you want to be added to the
+        waiting list, contact the right people.
       </p>
     </article>
   )
